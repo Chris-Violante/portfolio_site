d6d569a29af414e362653a35e270749a8af1d8e9
Christopher Violante <c.violante211@gmail.com>
2020-09-22 23:17:24 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App-Container">
+        <header className="App-Header">
+      {/* Nav will go here  */}
+        </header>
+        <main>
+          <div>Hello World I Am Chris Violante</div>
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App;
